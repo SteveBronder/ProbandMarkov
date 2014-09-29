431e6adb70dd055f541abe4ac30f37f703bc5e5b
stevo15025 <stevo15025@gmail.com>
2014-09-29 08:08:24 -0400

    Update Chapter 5

--- Chapter5.R
+++ Chapter5.R
@@ -65,10 +65,81 @@ A.matrix <- U.matrix%*%S.matrix
 
 # Make Full matrix
 
-Full.matrix <- matrix(c(1,0,0,0,0,
-                        0,1,0,0,0,
-                        .1,.3,0,.45,.15,
-                        .15,.1,0,.25,.5,
-                        .2,.05,0,.35,.4))
+Full.matrix <- matrix(c(1.00,0.00,0.00,0.00,0.00,
+                        0.00,1.00,0.00,0.00,0.00,
+                        0.10,0.30,0.00,0.45,0.15,
+                        0.15,0.10,0.00,0.25,0.50,
+                        0.20,0.05,0.00,0.35,0.40),5,5,byrow=TRUE)
 
-# Use above as example
+
+
+Tran.matrix <- Full.matrix[3:5,3:5]
+# Make identity
+Identity <- diag(1,3,3)
+# Use solve to find U.
+#  [1,1] is expected number of times you'll go to blood from blood
+U.matrix <- solve((Identity-Tran.matrix))
+
+apply(U.matrix,1,sum)
+
+
+#########
+# Find A#
+#########
+
+### Pull out Transient to recurrent portion portion of matrix (S)
+
+S.matrix <- Full.matrix[3:5,1:2]
+
+# [1,1] is prob of leaving from urine giving you started at blood
+# 22 percent chance of radioactive stuff exiting from urine
+A.matrix <- U.matrix%*%S.matrix
+
+################################
+################################
+######## Problem 1.15 ##########
+################################
+################################
+
+Full.matrix <- matrix(c(1.00,0.00,0.00,0.00,0.00,0.00,0.00,
+                        0.29,0.71,0.00,0.00,0.00,0.00,0.00,
+                        0.14,0.26,0.59,0.00,0.00,0.00,0.00,
+                        0.04,0.24,0.37,0.35,0.00,0.00,0.00,
+                        0.02,0.35,0.14,0.10,0.29,0.00,0.00,
+                        0.16,0.14,0.18,0.07,0.18,0.27,0.00,
+                        0.20,0.13,0.00,0.00,0.00,0.23,0.43)
+                        ,7,7,byrow=TRUE)
+
+Tran.matrix <- Full.matrix[2:7,2:7]
+                        
+
+# Make identity
+Identity <- diag(1,6,6)
+# Use solve to find U.
+#  [1,1] is expected number of times you'll go to blood from blood
+U.matrix <- solve((Identity-Tran.matrix))
+
+apply(U.matrix,1,sum)
+
+############################
+### Problem 5.16 ###########
+############################
+
+Full.matrix <- matrix(c(1.00,0.00,0.00,0.00,0.00,0.00,
+                        0.00,0.25,0.48,0.27,0.00,0.00,
+                        0.00,0.25,0.62,0.13,0.00,0.00,
+                        0.00,0.00,0.00,0.00,0.99,0.01,
+                        0.18,0.00,0.00,0.00,0.70,0.12,
+                        0.00,0.00,0.00,0.00,0.74,0.26)
+                      ,6,6,byrow=TRUE)
+
+Tran.matrix <- Full.matrix[2:6,2:6]
+
+
+# Make identity
+Identity <- diag(1,5,5)
+# Use solve to find U.
+#  [1,1] is expected number of times you'll go to blood from blood
+U.matrix <- solve((Identity-Tran.matrix))
+
+apply(U.matrix,1,sum)
